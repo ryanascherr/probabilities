43f0861d47351367656c2a4735b927f1ec78bb90
ryanascherr <ryanascherr@gmail.com>
2022-01-17 10:57:41 -0500

    Added logic for error messages.

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-$("h2").html(Math.floor(Math.random() * 10 + 1));
+$(".mod").val("+0");
+
+$(".calculate").click(function() {
+    let number = $(".number").val();
+    let dice = $(".size").val();
+    let mod = $(".mod").val();
+
+    if (!number || !dice || !mod) {
+        $(".error-message").html("You must fill in all inputs");
+        $(".result").html("");
+        return;
+    };
+    if (isNaN(number) || isNaN(dice) || isNaN(mod)) {
+        $(".error-message").html("All must be numbers");
+        $(".result").html("");
+        return;
+    }
+
+    $(".error-message").html("");
+   
+    $(".result").html(Math.floor(Math.random() * 10 + 1));
+});

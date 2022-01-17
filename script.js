e7fa820a1e8ff28163d90de0ee68a6158241b0c8
ryanascherr <ryanascherr@gmail.com>
2022-01-17 11:17:26 -0500

    Changed inputs to selects.

--- script.js
+++ script.js
@@ -1,22 +1,31 @@
+let number = '';
+let dice = '';
+let mod = '';
+let dc = '';
+
 $(".mod").val("+0");
 
 $(".calculate").click(function() {
-    let number = $(".number").val();
-    let dice = $(".size").val();
-    let mod = $(".mod").val();
+    number = $(".number").val();
+    dice = $(".size").val();
+    mod = $(".mod").val();
+    dc = $(".dc").val();
 
-    if (!number || !dice || !mod) {
-        $(".error-message").html("You must fill in all inputs");
-        $(".result").html("");
-        return;
-    };
+    handleErrorMessage();
+
+    handleCalculation();
+   
+});
+
+function handleErrorMessage() {
     if (isNaN(number) || isNaN(dice) || isNaN(mod)) {
         $(".error-message").html("All must be numbers");
         $(".result").html("");
         return;
     }
-
     $(".error-message").html("");
-   
+}
+
+function handleCalculation() {
     $(".result").html(Math.floor(Math.random() * 10 + 1));
-});
+}
